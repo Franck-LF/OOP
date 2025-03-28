f1e6369b0157232fbba57d60d58d830fbd5c732b
Franck-LF <f7lefur@gmail.com>
2025-03-28 14:36:48 +0100

    add loop to set album to each track

--- album.py
+++ album.py
@@ -9,7 +9,7 @@
 # afin qu’elle retourne automatiquement le nombre de pistes.
 
 
-import track
+from track import Track
 from media import Media 
 from typing import List, Dict
 
@@ -19,46 +19,51 @@ class Album(Media):
     ''' Class object representing an album
         contains all informations about album
         and methods to convert it into dictionary
-        and update an album from a dictionary. 
+        and update an album from a dictionary.
     '''
 
-    def __init__(self, title:str = '', release_year:int = 0, artist:str = '', tracks:int = 0, lst_tracks:List = None):
+    def __init__(self, title:str = '', release_year:int = 0, artist:str = '', tracks:List = []):
         ''' Album Constructor.
 
             Args:
              - title (string): title of the album,
              - release_year (int): year of release,
              - artist (string): with name of the artist,
-             - tracks (int): number of tracks in the album,
-             - lst_tracks (List(Track)): list of track objects.
+             - tracks (list(Track)): list of tracks in the album.
 
             title and release_year are initialized by the parent class (Media). 
         '''
         super().__init__(title, release_year, 'Album')
         self._artist = artist
-        self._tracks = tracks
-        self._lst_tracks = lst_tracks
+        self._tracks = []
+
+        for track in tracks:
+            assert isinstance(track, Track)
+            self._tracks.append(track)
+            track.set_album(self)
+
 
     def display_info(self):
         print('Album')
         print("title", self._title)
         print("release year", self._release_year)
-        print("nb tracks", self._tracks)
-        if self._lst_tracks:
+        print("nb tracks", len(self))
+        if self._tracks:
             print("lst_tracks", self._lst_tracks)
 
     def __str__(self) -> str:
-        if self._lst_tracks:
-            return f"Album: {self._title}, {self._release_year}, {self._artist}, {self._tracks}, {self._lst_tracks}"
-        return f"Album: {self._title}, {self._release_year}, {self._artist}, {self._tracks}"
+        st = f"Album: {self._title}, {self._release_year}, {self._artist}, nb tracks: {len(self)}"
+        for track in self._tracks:
+            st += f"\n{str(track)}"
+        return st
 
     def __len__(self) -> int:
-        return len(self._lst_tracks) if self._lst_tracks else 0
+        return len(self._tracks) if self._tracks else 0
 
     def to_dict(self) -> Dict:
         ''' convert album into dictionary '''
         dct_temp = super().to_dict()
-        dct_temp.update({'type' : 'Album', 'artist' : self._artist, 'tracks' : self._tracks, 'tracks' : self._lst_tracks})
+        dct_temp.update({'type' : 'Album', 'artist' : self._artist, 'tracks' : self._tracks})
         return dct_temp
 
     def from_dict(self, dct:Dict):
@@ -68,13 +73,18 @@ class Album(Media):
 
 
 
-
 if __name__ == '__main__':
-    album = Album('New Album', 2005, 'Lady Gaga', 12)
+    album = Album('New Album', 2005, 'Lady Gaga')
     print(album)
     print(album.to_dict())
+
+    track1 = Track('Biz', 195)
+    track2 = Track('Jeff', 202)
+    track3 = Track('Cece', 188)
+    lst_tracks = [track1, track2, track3]
+
     album.from_dict({'title' : 'update',
                      'release_year' : 2022,
                      'artist' : 'Amy Winehouse',
-                     'tracks' : 15})
+                     'tracks' : lst_tracks})
     print(album)
