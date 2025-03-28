969407971900a67b0850a7c7b32a009f1b183943
Franck-LF <f7lefur@gmail.com>
2025-03-28 13:44:03 +0100

    add comments about Track Class

--- track.py
+++ track.py
@@ -4,8 +4,15 @@
 
 
 class Track:
+    ''' Class object representing a track '''
 
     def __init__(self, title:str = '', length:int = 0):
+        ''' track Constructor 
+
+            Args:
+             - title (string): title of the track,
+             - length (int): duration of the track (in minutes),
+        '''
         self._title = title
         self._length = length
 
@@ -15,10 +22,14 @@ class Track:
         print("length", self._length)
 
     def __str__(self) -> str:
-        return f"Track {self._title}, {self._length}"
+        return f"Track: {self._title}, {self._length}"
 
     def to_dict(self):
         return {'title' : self._title, 'length' : self._length}
+
+    def from_dict(self, dct):
+        assert all([key in ('title', 'length') for key in dct.keys()])
+        self.__init__(dct['title'], dct['length'])
 
 
 
