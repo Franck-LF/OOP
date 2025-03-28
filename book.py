53105be4a50b9d0a192207fd1593e5eddd29f504
Franck-LF <f7lefur@gmail.com>
2025-03-28 13:42:43 +0100

    add comments about Book class

--- book.py
+++ book.py
@@ -3,11 +3,23 @@
 # Book(title, release_year, author, pages)
 
 from media import Media 
+from typing import List, Dict
 
 
 class Book(Media):
+    ''' Class object representing a book '''
 
     def __init__(self, title:str = '', release_year:int = 0, author:str = '', pages:int = 0):
+        ''' Book constructor.
+
+            Args:
+             - title (string): title of the album,
+             - release_year (int): year of release,
+             - author (string): author's name,
+             - pages (int): number of pages.
+
+            title and release_year are initialized by the parent class (Media).
+        '''
         super().__init__(title, release_year, 'Book')
         self._author = author
         self._pages = pages
@@ -22,12 +34,13 @@ class Book(Media):
     def __str__(self) -> str:
         return f"Book: {self._title}, {self._release_year}, {self._author}, {self._pages}"
 
-    def to_dict(self):
+    def to_dict(self) -> Dict:
+        ''' convert book into dictionary '''
         dct_temp = super().to_dict()
         dct_temp.update({'type' : 'Book', 'author' : self._author, 'pages' : self._pages})
         return dct_temp
 
-    def from_dict(self, dct):
+    def from_dict(self, dct:Dict):
         assert all([key in ('title', 'release_year', 'author', 'pages') for key in dct.keys()])
         self.__init__(dct['title'], dct['release_year'], dct['author'], dct['pages'])
 
